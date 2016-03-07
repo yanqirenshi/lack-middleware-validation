076773acffd5b6e2f90d8857b3694e204100525c
yanqirenshi <yanqirenshi@gmail.com>
2016-03-08 07:09:28 +0900

    add empty case fo validator-date

--- src/validators/date.lisp
+++ src/validators/date.lisp
@@ -1,10 +1,12 @@
 (in-package :lack.middleware.validation)
 
-(defun validator-date (name value &key (require nil) (default-value ""))
+(defun validator-date (name value &key (require nil) (default-value nil))
   (let ((value (validator-empty name value :require require :default-value default-value)))
-    (handler-case
-        (typecase value
-          (string (local-time:parse-timestring value))
-          (integer (local-time:universal-to-timestamp value))
-          (t (error "not supported value type")))
-      (error () (validation-error (format* nil :not-a-valid-date) name value)))))
+    (if (emptyp value)
+        nil
+        (handler-case
+            (typecase value
+              (string (local-time:parse-timestring value))
+              (integer (local-time:universal-to-timestamp value))
+              (t (error "not supported value type")))
+          (error () (validation-error (format* nil :not-a-valid-date) name value))))))
